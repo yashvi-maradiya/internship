1db034d3227e7e3f5a8b5587120dc7a28414b163
yashvi <yashvi@gmail.com>
2023-08-02 17:19:25 -0500

    initial commit

---
+++ pro1.js
@@ -0,0 +1,36 @@
+console.log("hello yashvi")
+// variable method
+let x= "apple" 
+console.log(x)
+x= "3788  "
+console.log(x)
+console.log(x[2])
+console.log(x.length)
+console.log(x/2)
+console.log(x*2)
+console.log(x**2)
+console.log(x+1234)
+// string method
+let y= "  Siyaram"
+console.log(y)
+console.log(y.length)
+//trim
+y= y.trim()  //only siyaram length count  
+console.log(y.length)
+//uppercase
+y= y.toUpperCase()
+console.log(y)
+// lowercase 
+y= y.toLowerCase()
+console.log(y)
+ 
+
+
+
+
+
+
+
+
+
+
